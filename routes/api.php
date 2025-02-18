51b9acb4fc00e1dbc5198b178f517a956998eed1
SRsoz <sozyar.rauf@chasacademy.se>
2025-02-18 17:13:33 +0100

    last commit

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
-
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChatbotController;
+use App\Http\Controllers\ChatHistoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,9 +9,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-route::post('/register',[AuthController::class, 'register']);
-route::post('/login',[Authcontroller::class, 'login']);
-route::get('/logout',[AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/register',[AuthController::class, 'register']);
+Route::post('/login',[Authcontroller::class, 'login']);
+Route::get('/logout',[AuthController::class, 'logout'])->middleware('auth:sanctum');
+route::post('/chat',[ChatbotController::class, 'chat'])->middleware('auth:sanctum');
+route::post('/chat',[ChatHistoryController::class, 'chat'])->middleware('auth:sanctum');
 
 Route::post('/test', function (Request $request) {
     $data = $request->test;
